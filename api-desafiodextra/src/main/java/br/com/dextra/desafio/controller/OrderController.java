388e6d5aa4a99fbfc6abb7aa5c24eed42db571aa
Felipe Carvalho <frkey@outlook.com>
2018-05-15 01:01:08 -0300

    add Order endpoints

--- api-desafiodextra/src/main/java/br/com/dextra/desafio/controller/OrderController.java
+++ api-desafiodextra/src/main/java/br/com/dextra/desafio/controller/OrderController.java
@@ -3,17 +3,25 @@ package br.com.dextra.desafio.controller;
 import java.util.List;
 
 import javax.inject.Inject;
+import javax.validation.Valid;
 import javax.ws.rs.core.MediaType;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+import org.springframework.web.util.UriComponents;
 
+import br.com.dextra.desafio.dto.request.OrderRequest;
 import br.com.dextra.desafio.dto.response.OrderResponse;
+import br.com.dextra.desafio.exception.NotFoundException;
 import br.com.dextra.desafio.service.OrderService;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
 import io.swagger.annotations.ApiResponse;
 import io.swagger.annotations.ApiResponses;
 
@@ -40,5 +48,62 @@ public class OrderController {
 	@RequestMapping(value = ORDERS_URI, method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON)
 	public ResponseEntity<List<OrderResponse>> fetchOrders() {
 		return ResponseEntity.ok(orderService.fetchAll());
+	}
+	
+	@ApiOperation(value = "Creates new order",
+			httpMethod = "POST", nickname = "createOrder", tags = "orders")
+	@ApiResponses(value = {
+			@ApiResponse(code = 201, message = "Item created"),
+			@ApiResponse(code = 400, message = "Invalid input, object invalid"),
+			@ApiResponse(code = 404, message = "Burger ID not found"),
+			@ApiResponse(code = 500, message = "Internal system error")})
+	@RequestMapping(value = ORDERS_URI, method = RequestMethod.POST, consumes = MediaType.APPLICATION_JSON,
+	produces = MediaType.APPLICATION_JSON)
+	public ResponseEntity<Void> create(
+			@ApiParam(value = "Order to create") @Valid @RequestBody OrderRequest order) throws NotFoundException {
+		OrderResponse saved = orderService.save(order);
+
+	    UriComponents resourceUri = ServletUriComponentsBuilder.fromCurrentRequest()
+	            .path("/{orderId}")
+	            .buildAndExpand(saved.getId());
+
+	    return ResponseEntity.created(resourceUri.toUri()).build();
+	}
+
+	@ApiOperation(value = "List all data about a order",
+			httpMethod = "GET", nickname = "getOrder", response = OrderResponse.class, tags = "orders")
+	@ApiResponses(value = { 
+        @ApiResponse(code = 200, message = "A single order", response = OrderResponse.class),
+        @ApiResponse(code = 404, message = "Order with this ID not found") })
+	@RequestMapping(value = ORDERS_URI + "/{orderId}", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON)
+	public ResponseEntity<OrderResponse> fetchOrder(
+			@ApiParam(value = "ID of order that needs to be fetched", required=true) @PathVariable("orderId") String id) throws NotFoundException {
+	    return ResponseEntity.ok(orderService.fetch(id));
+	}
+
+	@ApiOperation(value = "Replaces a order",
+			httpMethod = "PUT", nickname = "replaceOrder", tags = "orders")
+	@ApiResponses(value = {
+			@ApiResponse(code = 200, message = "Item replaced"),
+			@ApiResponse(code = 400, message = "Invalid input, object invalid"),
+			@ApiResponse(code = 404, message = "The order (or any of supplied IDs) is not found"),
+			@ApiResponse(code = 500, message = "Internal system error")})
+	@RequestMapping(value = ORDERS_URI + "/{orderId}", method = RequestMethod.PUT, consumes = MediaType.APPLICATION_JSON)	
+    public ResponseEntity<Void> replace(
+    		@ApiParam(value = "ID of order that needs to be replaced", required=true) @PathVariable("orderId") String id,
+    		@ApiParam(value = "Order to replace") @Valid @RequestBody OrderRequest orderRequest) throws NotFoundException {
+		orderService.update(orderRequest, id);
+	    return ResponseEntity.ok().build();    	
+    }
+
+	@ApiOperation(value = "Deletes a order", httpMethod = "DELETE", nickname = "deleteIngredient", tags = "orders")
+    @ApiResponses(value = { 
+	        @ApiResponse(code = 200, message = "Item deleted"),
+	        @ApiResponse(code = 404, message = "The order is not found") })	
+	@RequestMapping(value = ORDERS_URI + "/{orderId}", method = RequestMethod.DELETE)
+    public ResponseEntity<Void> deleteIngredient(
+    		@ApiParam(value = "ID of order that needs to be deleted", required=true) @PathVariable("orderId") String id) throws NotFoundException {
+		orderService.delete(id);
+	    return ResponseEntity.ok().build();
 	}
 }
--- api-desafiodextra/src/main/java/br/com/dextra/desafio/service/OrderService.java
+++ api-desafiodextra/src/main/java/br/com/dextra/desafio/service/OrderService.java
@@ -1,6 +1,7 @@
 package br.com.dextra.desafio.service;
 
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -15,6 +16,7 @@ import br.com.dextra.desafio.dto.request.OrderRequest;
 import br.com.dextra.desafio.dto.response.BurgerResponse;
 import br.com.dextra.desafio.dto.response.IngredientResponse;
 import br.com.dextra.desafio.dto.response.OrderResponse;
+import br.com.dextra.desafio.exception.NotFoundException;
 import br.com.dextra.desafio.repository.OrderRepository;
 
 @Service
@@ -31,8 +33,68 @@ public class OrderService {
         return repository.findAll().stream().map(c -> buildResponse(c)).collect(Collectors.toList());
     }
 
+	public OrderResponse save(final OrderRequest orderRequest) {
+		Order order = buildFromRequest(orderRequest);
+		order.setCreationDate(new Date());
+		
+        Order savedOrder = repository.save(order);
+        return buildResponse(savedOrder);
+    }
+
+	public OrderResponse fetch(String id) throws NotFoundException {
+        Order order = repository.findOne(id);
+        if (order == null) {
+            throw new NotFoundException("Order not found");
+        }
+        return buildResponse(order);
+    }
+
+    public OrderResponse update(OrderRequest orderRequest, String id) throws NotFoundException {
+        OrderResponse orderResponse = fetch(id);
+
+        Order order = buildFromRequest(orderRequest);
+        order.setId(orderResponse.getId());
+
+        return buildResponse(repository.save(order));
+    }
+
+    public void delete(String id) throws NotFoundException {
+        fetch(id);
+        repository.delete(id);
+    }
+
 	private Order buildFromRequest(OrderRequest orderRequest) {
-        return null;
+		List<Burger> burgers = null;
+		List<Ingredient> ingredients;
+		List<IngredientResponse> ingredientsResponse = null;
+		final List<BurgerResponse> burgersResponse = orderRequest.getBurgers();
+		
+		if (burgersResponse != null) {
+			burgers = new ArrayList<>();
+			for (BurgerResponse burgerResponse : burgersResponse) {
+				ingredientsResponse = burgerResponse.getIngredients();
+				ingredients = null;
+				
+				if (ingredientsResponse != null) {
+					ingredients = new ArrayList<>();
+					for (IngredientResponse ingredientResponse : ingredientsResponse)
+						ingredients.add(Ingredient.builder()
+									.id(ingredientResponse.getId())
+									.name(ingredientResponse.getName())
+									.price(ingredientResponse.getPrice())
+									.build());
+				}				
+				burgers.add(Burger.builder()
+						.id(burgerResponse.getId())
+						.name(burgerResponse.getName())
+						.ingredients(ingredients)
+						.build());
+			}
+		}
+		
+        return Order.builder()
+        		.burgers(burgers)
+        		.build();
     }
 
     private OrderResponse buildResponse(Order order) {
