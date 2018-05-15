0b2c366257c7b812858053de97341559f3495e32
Felipe Carvalho <frkey@outlook.com>
2018-05-15 08:47:55 -0300

    fix swagger

--- api-desafiodextra/src/main/java/br/com/dextra/desafio/DesafioApplication.java
+++ api-desafiodextra/src/main/java/br/com/dextra/desafio/DesafioApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
-@EnableSwagger2
 @SpringBootApplication(scanBasePackages = {"br.com.dextra.desafio"})
 public class DesafioApplication {
 
